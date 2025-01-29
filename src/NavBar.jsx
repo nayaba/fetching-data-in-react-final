116e99488e87d8a614813f82de26dd46285dad07
Nabila Ayaba <nabilaayaba@gmail.com>
2025-01-29 20:23:28 +0300

    Add sass and icons

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,9 @@ const NavBar = () => {
     return (
 <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">
+        <i className="bi bi-virus2"></i>
+    </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
