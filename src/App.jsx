484fbe03062907079faf55587af2e69c1d420fbc
Nabila Ayaba <nabilaayaba@gmail.com>
2025-01-29 20:15:16 +0300

    Add Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import * as studentService from './services/studentService'
+import NavBar from './NavBar'
 
 const App = () => {
 
@@ -23,14 +24,16 @@ const App = () => {
 
   return (
     <>
+    <NavBar />
       <h1>Fetch All the Students</h1>
       <ul>
         {students.map((student) => (
           <li key={student._id}>{student.name}
-          <button onClick={() => handleDelete(student._id)}>X</button>
+          <button className='btn btn-danger' onClick={() => handleDelete(student._id)}>X</button>
           </li>
         ))}
       </ul>
+      <button className='btn btn-warning'>Submit</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import './custom.scss'
+import 'bootstrap'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
